e86ac85a613a29eac38218060f40e230ed1408b4
Rodrigo Drummond Macedo <rodrigodm2810@gmail.com>
2025-08-11 22:35:49 -0300

    fix: adicionando barra ao final da url da api

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import axios from 'axios';
 
-const API_URL = 'https://feedback-backend-f5gqena4dahbbafm.eastus2-01.azurewebsites.net/api';
+const API_URL = 'https://feedback-backend-f5gqena4dahbbafm.eastus2-01.azurewebsites.net/api/';
 
 const api = axios.create({
   baseURL: API_URL,
